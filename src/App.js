eec0c8720fd27d888929811e04084ef98cf38a52
DevDogFe <jhkim900726@gmail.com>
2023-06-30 00:20:46 +0900

    react 공부 시작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+// JSX 문법
+// class => className으로 변경
+// 변수사용 {}
+// 인라인 스타일 사용 => style = {{ ... }}
+// return() 괄호 안에는 두가지 이상태그 병렬사용 x
 function App() {
+  /**
+   * - state
+   * 1. import {useState}
+   * 2. useState(보관할 변수)
+   * 3. let [이름1, 이름2] <-에 담기 (이름1: state에 보관한 변수, 이름2: state 변경 도와주는 함수)
+   */
+  let post = '전포 카페거리 양고기 무한리필 맛집';
+  let [title, bb] = useState(['올해의 아이템 쿨니트', '무신사에서 아이템 찾는 법', '서면 중국집 맛집 추천']);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4>MyBlog</h4>
+      </div>
+      <div className='list'>
+        <h4>{post}</h4>
+        <p>2023-06-29</p>
+      </div>
+      <div className='list'>
+        <h4>{title[0]}</h4>
+        <p>2023-06-29</p>
+      </div>
+      <div className='list'>
+        <h4>{title[1]}</h4>
+        <p>2023-06-29</p>
+      </div>
+      <div className='list'>
+        <h4>{title[2]}</h4>
+        <p>2023-06-29</p>
+      </div>
     </div>
   );
 }
